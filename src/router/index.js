47448c1037d6e6dd8e01c544a140cdd01c21d450
“PeggyTsai” <pinklover1671291991@gmail.com>
2022-02-25 16:59:21 +0800

    import bootstrap

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,21 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: () => import('../views/FrontView.vue'),
+    children: [
+      {
+        path: '',
+        name: 'HomeView',
+        component: HomeView
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/admin',
+    component: () => import('../views/DashboardView.vue')
   }
 ]
 
